636f8c9644b2fdd1e43db71283d4e2040801b0e1
caroline lopes <161317565+lpmtm@users.noreply.github.com>
2025-06-02 09:12:43 -0300

    Update and rename lista_2_exercicio1.c to apc1/lista_2_exercicio1.c

--- apc1/lista_2_exercicio1.c
+++ apc1/lista_2_exercicio1.c
@@ -1,7 +1,7 @@
 //cabecalho:
 	//nome: caroline lopes martins (matricula: 2412130073)
 	//lista 2- exercicio 1
-	//objetivo: programa para ver se é possivel formar triangulos e classifica-los
+	//objetivo: programa para ver se Ã© possivel formar triangulos e classifica-los
 	//data: 09/05/2024
 
 #include <stdio.h>
@@ -24,23 +24,23 @@ int main(){
 	scanf ("%d", &z);
 	
 	if (x+y>z){
-		printf ("\né possivel formar o triangulo!");
+		printf ("\nÃ© possivel formar o triangulo!");
 	}
 	if (x+z>y){
-		printf ("\né possivel formar o triangulo!");
+		printf ("\nÃ© possivel formar o triangulo!");
 	}
 	if (y+z>x){
-		printf ("\né possivel formar o triangulo!");
+		printf ("\nÃ© possivel formar o triangulo!");
 	}	
 	
 	if (x=y=z){ 
-		printf ("\nseu triangulo é umm triangulo equilatero!");
+		printf ("\nseu triangulo Ã© umm triangulo equilatero!");
 	}else{
-		printf ("\nseu triangulo é um triangulo escaleno!");
+		printf ("\nseu triangulo Ã© um triangulo escaleno!");
 	}
 		
 	for (x=y; y=z; z=x);{
-		printf ("\nseu triangulo é um triangulo isoceles!");
+		printf ("\nseu triangulo Ã© um triangulo isoceles!");
 	}
 	printf ("\nCaroline Lopes matric. 2412130073");
 	return 0;
